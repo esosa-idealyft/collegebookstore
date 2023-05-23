3dabfd7d958a725ca187fe258370d0f4bab1fcfa
esosa-idealyft <134346075+esosa-idealyft@users.noreply.github.com>
2023-05-22 18:09:12 -0700

    Add files via upload

---
+++ CollegeBookstore.Designer.vb
@@ -0,0 +1,200 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class CollegeBookstore
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        ListBox1 = New ListBox()
+        Label2 = New Label()
+        Button2 = New Button()
+        Button1 = New Button()
+        Button3 = New Button()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        Label3 = New Label()
+        Label4 = New Label()
+        RadioButton1 = New RadioButton()
+        RadioButton2 = New RadioButton()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(48, 6)
+        Label1.Name = "Label1"
+        Label1.RightToLeft = RightToLeft.Yes
+        Label1.Size = New Size(259, 25)
+        Label1.TabIndex = 0
+        Label1.Text = "Please Select An Access Code"
+        ' 
+        ' ListBox1
+        ' 
+        ListBox1.FormattingEnabled = True
+        ListBox1.ImeMode = ImeMode.NoControl
+        ListBox1.ItemHeight = 25
+        ListBox1.Items.AddRange(New Object() {"Visio", "QuickBooks", "Project", "PowerBi", "Speed Type"})
+        ListBox1.Location = New Point(58, 34)
+        ListBox1.Name = "ListBox1"
+        ListBox1.ScrollAlwaysVisible = True
+        ListBox1.Size = New Size(570, 129)
+        ListBox1.TabIndex = 1
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(46, 180)
+        Label2.Name = "Label2"
+        Label2.RightToLeft = RightToLeft.Yes
+        Label2.Size = New Size(85, 25)
+        Label2.TabIndex = 2
+        Label2.Text = "?Student"
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(66, 474)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(271, 57)
+        Button2.TabIndex = 3
+        Button2.Text = "Clear Form"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(241, 334)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(271, 57)
+        Button1.TabIndex = 4
+        Button1.Text = "Final Total"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(379, 474)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(271, 57)
+        Button3.TabIndex = 3
+        Button3.Text = "Daily Total"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(47, 290)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(335, 33)
+        TextBox1.TabIndex = 5
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(423, 290)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(134, 33)
+        TextBox2.TabIndex = 5
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(206, 411)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(335, 33)
+        TextBox3.TabIndex = 5
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(47, 262)
+        Label3.Name = "Label3"
+        Label3.RightToLeft = RightToLeft.Yes
+        Label3.Size = New Size(247, 25)
+        Label3.TabIndex = 6
+        Label3.Text = "Buyer's Credit Card Number"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(423, 262)
+        Label4.Name = "Label4"
+        Label4.RightToLeft = RightToLeft.Yes
+        Label4.Size = New Size(307, 25)
+        Label4.TabIndex = 6
+        Label4.Text = "Buyer's Credit Card Expiration Date"
+        ' 
+        ' RadioButton1
+        ' 
+        RadioButton1.AutoSize = True
+        RadioButton1.Location = New Point(58, 213)
+        RadioButton1.Name = "RadioButton1"
+        RadioButton1.Size = New Size(64, 29)
+        RadioButton1.TabIndex = 7
+        RadioButton1.TabStop = True
+        RadioButton1.Text = "Yes"
+        RadioButton1.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton2
+        ' 
+        RadioButton2.AutoSize = True
+        RadioButton2.Location = New Point(168, 213)
+        RadioButton2.Name = "RadioButton2"
+        RadioButton2.Size = New Size(62, 29)
+        RadioButton2.TabIndex = 7
+        RadioButton2.TabStop = True
+        RadioButton2.Text = "No"
+        RadioButton2.UseVisualStyleBackColor = True
+        ' 
+        ' CollegeBookstore
+        ' 
+        AutoScaleDimensions = New SizeF(11.0F, 25.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 561)
+        Controls.Add(RadioButton2)
+        Controls.Add(RadioButton1)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox1)
+        Controls.Add(Button1)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Label2)
+        Controls.Add(ListBox1)
+        Controls.Add(Label1)
+        ImeMode = ImeMode.On
+        Name = "CollegeBookstore"
+        Text = "College Bookstore"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+End Class
---
+++ CollegeBookstore.vb
@@ -0,0 +1,169 @@
+﻿Imports System.ComponentModel
+Imports System.IO
+
+Public Class CollegeBookstore
+
+    Private Sub CollegeBookstore_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'ListBox1.Items.Add(“Visual Basic 6”)
+    End Sub
+
+
+    Private Sub Button2_Click_1(sender As Object, e As EventArgs) Handles Button2.Click
+
+        Me.ListBox1.ClearSelected()
+        Me.TextBox1.Text = ""
+        Me.TextBox2.Text = ""
+        Me.TextBox3.Text = ""
+        'Me.TextBox3.Text = vbEmpty
+        Me.RadioButton1.Checked = False
+        Me.RadioButton2.Checked = False
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        'Step 1 - Get Price
+
+        Dim selectedvalue As String
+        Dim PriceHere As Double
+        Dim PriceDisct, DsctAmt As Double
+        Dim ApplyDisct As String
+        Dim FinalDisct As String
+        Dim FinalPrice As String
+
+
+        If Me.RadioButton1.Checked Then
+
+            ApplyDisct = "Yes"
+
+        Else : End If
+
+        selectedvalue = Me.ListBox1.SelectedItem
+
+
+        If selectedvalue = "Visio" Then
+
+            PriceHere = 14.99
+            DsctAmt = 0.06
+            PriceDisct = PriceHere * DsctAmt
+
+
+        ElseIf selectedvalue = "QuickBooks" Then
+
+            PriceHere = 24.99
+            DsctAmt = 0.08
+            PriceDisct = PriceHere * DsctAmt
+
+
+        ElseIf selectedvalue = "Project" Then
+
+            PriceHere = 17.99
+            DsctAmt = 0.08
+            PriceDisct = PriceHere * DsctAmt
+
+
+        ElseIf selectedvalue = "PowerBi" Then
+
+            PriceHere = 9.99
+            DsctAmt = 0.05
+            PriceDisct = PriceHere * DsctAmt
+
+        ElseIf selectedvalue = "SpeedType" Then
+
+            PriceHere = 29.99
+            DsctAmt = 0.02
+            PriceDisct = PriceHere * DsctAmt
+
+
+        End If
+
+        If ApplyDisct = "Yes" Then
+
+            PriceHere = PriceHere - PriceDisct
+
+        Else
+
+            PriceDisct = 0.00
+
+        End If
+
+        FinalPrice = Format(PriceHere, "#.##")
+        'FinalDisct = Format(PriceDisct, "#.##")
+
+
+
+        'Display Final Price
+
+        Me.TextBox3.Text = FinalPrice
+
+        'STEP 2 - Create Line Item for Text File
+
+        Dim newdatastring As String
+        newdatastring = Me.TextBox1.Text & "," & selectedvalue & "," & FinalDisct & "," & FinalPrice
+        'newdatastring = "CC:" & Me.TextBox1.Text & ",App:" & selectedvalue & ",Dsct:" & FinalDisct & ",Amt:" & Me.TextBox3.Text
+        Dim oldata As String
+
+        'Step 3 - Add to Text File
+
+        'If Text File does not exist, create one...
+
+        Dim path As String
+        Dim username As String
+        username = Environment.UserName
+        Dim filedate As String = DateTime.Now.ToString("MM.dd.yy")
+        path = "C:\Users\" & username & "\Documents\" & "CollegeSales_" & filedate & ".txt"
+
+        'Add Data to Text File
+
+        If (File.Exists(path)) Then
+
+            'oldata = My.Computer.FileSystem.ReadAllText(path)
+            'My.Computer.FileSystem.WriteAllText(path, oldata & vbCrLf & newdatastring, True)
+            File.AppendAllText(path, Environment.NewLine + newdatastring)
+        Else
+
+            My.Computer.FileSystem.WriteAllText(path, newdatastring, True)
+
+        End If
+
+        My.Computer.Clipboard.SetText(path)
+
+        MsgBox("File has been saved to & copied to your clipboard: " & path)
+
+        'Open File
+
+        'My.Computer.FileSystem.OpenTextFileReader(path)
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        Dim path As String
+        Dim filedate As String = DateTime.Now.ToString("MM.dd.yy")
+        Dim username As String
+        username = Environment.UserName
+        path = "C:\Users\" & username & "\Documents\" & "CollegeSales_" & filedate & ".txt"
+
+        'Sum everything after Amt:
+        Dim filecontents As String() = IO.File.ReadAllLines(path)
+        Dim Sum As Double = 0 ' This holds the sum
+        For Each line In filecontents
+            ' Using comma ',' as a separator (you may use other characters)
+            Dim tokens() As String = line.Split(New Char() {","c})
+            Dim value As Double = 0 ' This holds the current value
+            ' Let's suppose you need to add up a third column of integer values:
+            If tokens.Length >= 3 Then
+                ' Parsing the third column and if successful add it to the sum:
+                If Double.TryParse(tokens(3), value) Then
+                    Sum += value
+                End If
+            End If
+        Next
+
+
+        MsgBox("Total Daily Sales: $" & Sum)
+
+
+    End Sub
+End Class
